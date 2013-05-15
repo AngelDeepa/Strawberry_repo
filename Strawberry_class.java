5cc61666b987b94d075e22cd9d7d40c862c0dbda
dmurugesanatpros <dmurugesan@pros.com>
2013-05-15 14:20:39 +0530

    first git coomit

---
+++ Strawberry_class.java
@@ -0,0 +1,13 @@
+package strawbery_package;
+
+public class Strawberry_class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("M strawbeeery class");
+
+	}
+
+}
